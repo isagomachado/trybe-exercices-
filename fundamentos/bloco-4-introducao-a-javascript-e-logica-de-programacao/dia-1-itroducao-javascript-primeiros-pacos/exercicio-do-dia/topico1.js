f2d64b1e19d2241d32d6b008e27b33335d690109
isagomachado <isa_gomes_machado@hotmail.com>
2021-11-26 20:42:29 -0300

    topico 1 operadores matematicos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-itroducao-javascript-primeiros-pacos/exercicio-do-dia/topico1.js
@@ -0,0 +1,30 @@
+const a = 5
+const b = 13 
+
+// Adição
+
+let soma = a + b
+
+//Subtração
+
+let sub = a - b
+
+//Multiplicação
+
+let mult = a * b
+
+//divisão
+
+let divd = a / b
+
+//Modulo
+
+let mod = a % b
+
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(divd)
+console.log(mod)
+
