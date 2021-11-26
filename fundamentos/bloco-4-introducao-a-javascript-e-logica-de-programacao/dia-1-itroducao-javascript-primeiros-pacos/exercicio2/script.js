fa1b9c4b02e6dd1cea337c0181702f175ecbbbcd
isagomachado <isa_gomes_machado@hotmail.com>
2021-11-26 18:13:51 -0300

    exercicio um mais pasta e aquivo para o exercicio 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-itroducao-javascript-primeiros-pacos/exercicio2/script.js
@@ -0,0 +1,14 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+// console.log(typeof patienteAge)
+
+patientId = "50"
+
+// console.log(typeof patientId)
+
