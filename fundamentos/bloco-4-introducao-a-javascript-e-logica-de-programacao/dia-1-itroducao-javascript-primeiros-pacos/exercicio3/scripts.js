d3c5bd8149412a27c8e2794d7d4c049e009863e9
isagomachado <isa_gomes_machado@hotmail.com>
2021-11-26 18:41:02 -0300

    exercicio 3 - condicionais

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-itroducao-javascript-primeiros-pacos/exercicio3/scripts.js
@@ -0,0 +1,9 @@
+let notaDesafio = 52;
+
+if (notaDesafio >= 80){
+    console.log("Parabéns, você foi aprovado")
+} else if (notaDesafio < 80 && notaDesafio >= 60){
+    console.log("Você está na nossa lista de espera")
+} else {
+    console.log("Você foi reprovado")
+}
