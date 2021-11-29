7d634c9b542e1b45db3968c43b1adcf299a0c423
isagomachado <isa_gomes_machado@hotmail.com>
2021-11-29 15:51:19 -0300

    exercicios sobre array 1, 2 e 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicios-fixacao/arrays(lista).js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicios-fixacao/arrays(lista).js
@@ -1,3 +1,5 @@
+// Primeira parte
+
 let tasksList = ["Tomar café", "Reunião", "brincar com o cachorro"];
 console.log(tasksList.length);
 
@@ -6,3 +8,35 @@ console.log(searchForFirstTask);
 
 let searchForLastTask = tasksList[tasksList.length - 1]
 console.log(searchForLastTask)
+
+tasksList.push("Fazer exercícios da Trybe")
+console.log(tasksList)
+
+tasksList.unshift("Sair da cama")
+console.log(tasksList)
+
+tasksList.pop()
+console.log(tasksList)
+
+let indexOfTask = tasksList.indexOf("Reunião")
+console.log(indexOfTask)
+
+// Segunda Parte
+
+//1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf("Serviços");
+
+console.log(menuServices);
+
+//2
+
+let indexOfPortfolio = menu.indexOf("Portfólio");
+
+console.log(indexOfPortfolio);
+
+//3
+
+menu.push("Contato")
+console.log(menu)
