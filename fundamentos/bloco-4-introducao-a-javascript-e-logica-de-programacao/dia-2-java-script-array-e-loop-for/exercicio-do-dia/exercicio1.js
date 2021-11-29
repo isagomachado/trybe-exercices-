0861ce52b19a077386d4ef10925556c3c739af90
isagomachado <isa_gomes_machado@hotmail.com>
2021-11-29 18:32:51 -0300

    exercicios 3 e 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicio-do-dia/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicio-do-dia/exercicio1.js
@@ -1,14 +1,33 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //exercicio1
-for (let i = 0; i < numbers.length; i += 1){
-  console.log(numbers[i])
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i]);
 }
 
 //exercicio2
-let soma = 0
+let soma = 0;
 
-for (let index = 0; index < numbers.length; index += 1){
+for (let index = 0; index < numbers.length; index += 1) {
   soma = soma + numbers[index];
 }
-console.log("A soma dos numeros dentro da array é:", soma)
+console.log("A soma dos numeros dentro da array é:", soma);
+
+//exercicio3
+let soma2 = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+  soma2 = soma2 + numbers[i];
+}
+
+let media = soma2 / numbers.length
+
+console.log("A média aritmética é:", media)
+
+//exercicio4 - complemento do codigo anterior
+
+if (media > 20){
+  console.log("Valor maior que 20")
+}else {
+  "Valor menor ou igual a 20"
+}
