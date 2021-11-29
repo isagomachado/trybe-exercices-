2586f3512b7a5167b88fe19e9e2a2d2600efd82c
isagomachado <isa_gomes_machado@hotmail.com>
2021-11-29 15:34:50 -0300

    primeiro e ultimo elemtos da tasklist

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicios-fixacao/arrays(lista).js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicios-fixacao/arrays(lista).js
@@ -1,4 +1,8 @@
 let tasksList = ["Tomar café", "Reunião", "brincar com o cachorro"];
-console.log(tasksList.length)
+console.log(tasksList.length);
 
-let searchForFirstTask = tasksList[0]
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+
+let searchForLastTask = tasksList[tasksList.length - 1]
+console.log(searchForLastTask)
