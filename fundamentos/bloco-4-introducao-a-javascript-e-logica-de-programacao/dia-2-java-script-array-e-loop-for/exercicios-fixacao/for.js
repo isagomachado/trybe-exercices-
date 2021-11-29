dbae1e7dba14d817a1f5b9a271b3a5c38083b71a
isagomachado <isa_gomes_machado@hotmail.com>
2021-11-29 16:10:40 -0300

    exercicio 1 for

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicios-fixacao/for.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicios-fixacao/for.js
@@ -1,1 +1,12 @@
-let cars = ['Saab', 'Volvo', 'BMW'];
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index = 0; index < cars.length; index += 1){
+//   console.log(cars[index])
+// }
+
+// exercicio 1
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let i = 0; i < groceryList.length; i += 1){
+  console.log(groceryList[i])
+}
