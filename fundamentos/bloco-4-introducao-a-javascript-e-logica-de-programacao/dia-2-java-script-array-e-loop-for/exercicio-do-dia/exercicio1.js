be33c971ece785e5e392324630223d0f551fe66c
isagomachado <isa_gomes_machado@hotmail.com>
2021-11-29 18:22:46 -0300

    exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicio-do-dia/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicio-do-dia/exercicio1.js
@@ -4,3 +4,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let i = 0; i < numbers.length; i += 1){
   console.log(numbers[i])
 }
+
+//exercicio2
+let soma = 0
+
+for (let index = 0; index < numbers.length; index += 1){
+  soma = soma + numbers[index];
+}
+console.log("A soma dos numeros dentro da array é:", soma)
