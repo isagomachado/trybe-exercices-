c89ed264832e8469b97f31f7222d2567520fbd14
isagomachado <isa_gomes_machado@hotmail.com>
2021-11-30 14:58:20 -0300

    corrigi exercicio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicio-do-dia/exercicio6.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicio-do-dia/exercicio6.js
@@ -1,13 +1,16 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let impares = []
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27,4];
+let impares = [];
 
- for (let i = 0; i < numbers.length; i += 1){
-   if (numbers[i] % 2 !== 0){
-     impares.push(numbers[i])
-     
-   }else{
-    impares.push("Nenhum valor encontrado")
- }
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] % 2 !== 0) {
+    impares.push(numbers[i]);
+  } else if (impares.length === 0 && i === numbers.length -1 ) {
+    console.log("nenhum valor ímpar encontrado");
+  }
+
+  if(i === numbers.length -1){
+    console.log(impares);
+  }
 }
- 
- console.log(impares)
+
+
