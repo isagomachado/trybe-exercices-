5611649d2bafe6fc86f71ee729d1672581a32396
isagomachado <isa_gomes_machado@hotmail.com>
2021-12-01 12:37:34 -0300

    exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-java-script-logica-de-programacao-e-algoritmo/exercicio-do-dia/exercicio3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-java-script-logica-de-programacao-e-algoritmo/exercicio-do-dia/exercicio3.js
@@ -1,1 +1,21 @@
 let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array[0] 
+let menorPalavra = array[0]
+
+//maior palavra 
+for (let index = 0; index < array.length; index += 1){
+  if(array[index].length > maiorPalavra.length){
+    maiorPalavra = array[index]
+  }
+}
+
+console.log(maiorPalavra)
+
+//menor palavra 
+for (let index = 0; index < array.length; index += 1){
+  if(array[index].length < menorPalavra.length){
+    menorPalavra = array[index]
+  }
+}
+
+console.log(menorPalavra)
