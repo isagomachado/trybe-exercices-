db1ee0a3cff14fdcf85e2bd9d810ad65b4256b7f
isagomachado <isa_gomes_machado@hotmail.com>
2021-12-01 15:58:36 -0300

    topico 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-java-script-objetos-e-funcoes/exericios-fixacao/objetos/exericio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-java-script-objetos-e-funcoes/exericios-fixacao/objetos/exericio1.js
@@ -10,10 +10,24 @@ let player = {
 
 let mensagem =
   "A jogadora " +
-  player.name + " " +
+  player.name +
+  " " +
   player.lastName +
   " tem " +
   player.age +
   "anos de idade";
 
-  console.log(mensagem)
+console.log(mensagem);
+
+player.bestInTheWord = [2006, 2007, 2008, 2009, 2010, 2018];
+
+let mensagem2 =
+  "A jogadora " +
+  player.name +
+  " " +
+  player.lastName +
+  " foi eleita a melhor jogadora do mundo por " +
+  player["bestInTheWord"].length +
+  " vezes!";
+
+  console.log(mensagem2)
