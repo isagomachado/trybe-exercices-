1eca9d0ae79a80ee0d0f8162e78015e44638abe3
isagomachado <isa_gomes_machado@hotmail.com>
2021-12-01 15:36:58 -0300

    topicos 1 e 2 exercicio de fixação objetos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-java-script-objetos-e-funcoes/exericios-fixacao/objetos/exericio1.js
@@ -0,0 +1,19 @@
+let player = {
+  name: "Marta",
+  lastName: "Silva",
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  },
+};
+
+let mensagem =
+  "A jogadora " +
+  player.name + " " +
+  player.lastName +
+  " tem " +
+  player.age +
+  "anos de idade";
+
+  console.log(mensagem)
