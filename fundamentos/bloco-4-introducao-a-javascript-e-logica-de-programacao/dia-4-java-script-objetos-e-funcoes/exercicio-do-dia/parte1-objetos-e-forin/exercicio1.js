a22995cad05b3b5cdb68870b174fae7a57eef5e3
isagomachado <isa_gomes_machado@hotmail.com>
2021-12-02 10:05:49 -0300

    exericicio 1 , mensagem para margarida

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-java-script-objetos-e-funcoes/exercicio-do-dia/parte1-objetos-e-forin/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-java-script-objetos-e-funcoes/exercicio-do-dia/parte1-objetos-e-forin/exercicio1.js
@@ -1,5 +1,13 @@
+//exercicio1
+
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
+
+let mensagem = "Bem-vinda, " + info.personagem
+console.log(mensagem)
+
+//exercicio2
+
