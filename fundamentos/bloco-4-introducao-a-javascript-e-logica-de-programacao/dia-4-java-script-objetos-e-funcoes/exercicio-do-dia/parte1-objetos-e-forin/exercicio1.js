bebf05f4baae1030cafd7f6603c6d45203d215f7
isagomachado <isa_gomes_machado@hotmail.com>
2021-12-02 10:59:54 -0300

    todos os exercicios

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-java-script-objetos-e-funcoes/exercicio-do-dia/parte1-objetos-e-forin/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-java-script-objetos-e-funcoes/exercicio-do-dia/parte1-objetos-e-forin/exercicio1.js
@@ -6,17 +6,43 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-let mensagem = "Bem-vinda, " + info.personagem
-console.log(mensagem)
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem:"Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "sim",
+};
+
+let mensagem = "Bem-vinda, " + info.personagem;
+console.log(mensagem);
 
 //exercicio2
 
-info.recorrente = "sim"
+info.recorrente = "sim";
 
-console.log(info)
+console.log(info);
 
 //exercicio3
 
 for (let key in info){
   console.log(key)
-}
+};
+
+//exercicio4
+
+for (let key in info){
+  console.log(info[key])
+};
+
+//exercicio5
+
+console.log(info.personagem + " e " + info2.personagem);
+console.log(info.origem + " e " + info2.origem);
+console.log(info.nota + " e " + info2.nota);
+
+let lastMessage;
+
+if (info.recorrente === "sim" && info2.recorrente === "sim");
+
+
+console.log();
