0411cc265058b205d1228a57d53ff6f7b2e72f8c
isagomachado <isa_gomes_machado@hotmail.com>
2021-12-02 08:52:42 -0300

    funções envolvendo operadores artiméticos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-java-script-objetos-e-funcoes/exericios-fixacao/funcoes/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-java-script-objetos-e-funcoes/exericios-fixacao/funcoes/exercicio1.js
@@ -1,3 +1,30 @@
 //Refatorando o código dos 5 primeiros exericios do dia 4.1
 
 //topico1
+
+function soma(a,b) {
+  return a + b
+}
+
+function subtrai(a,b) {
+  return a - b
+}
+
+function multiplica(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function modulo(a,b) {
+  return a % b
+}
+
+console.log(soma(5,13))
+console.log(subtrai(5,13))
+console.log(multiplica(5,13))
+console.log(divide(5,13))
+console.log(modulo(5,13))
+
