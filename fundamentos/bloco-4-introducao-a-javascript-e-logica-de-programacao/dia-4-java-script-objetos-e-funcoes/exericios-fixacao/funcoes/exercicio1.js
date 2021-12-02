3e839ad2cb9830011624a1d785b71f1f9c933c8c
isagomachado <isa_gomes_machado@hotmail.com>
2021-12-02 09:20:08 -0300

    topico 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-java-script-objetos-e-funcoes/exericios-fixacao/funcoes/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-java-script-objetos-e-funcoes/exericios-fixacao/funcoes/exercicio1.js
@@ -2,62 +2,85 @@
 
 //topico1
 
-function soma(a,b) {
-  return a + b
+function soma(a, b) {
+  return a + b;
 }
 
-function subtrai(a,b) {
-  return a - b
+function subtrai(a, b) {
+  return a - b;
 }
 
-function multiplica(a,b) {
-  return a * b
+function multiplica(a, b) {
+  return a * b;
 }
 
-function divide(a,b) {
-  return a / b
+function divide(a, b) {
+  return a / b;
 }
 
-function modulo(a,b) {
-  return a % b
+function modulo(a, b) {
+  return a % b;
 }
 
-console.log(soma(5,13))
-console.log(subtrai(5,13))
-console.log(multiplica(5,13))
-console.log(divide(5,13))
-console.log(modulo(5,13))
-
+console.log(soma(5, 13));
+console.log(subtrai(5, 13));
+console.log(multiplica(5, 13));
+console.log(divide(5, 13));
+console.log(modulo(5, 13));
 
 //topico2
 
-function maiorNumero(a,b) {
-
-  if (a > b){
-    return a 
-  } else{
-    return b
+function maiorNumero(a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
   }
 }
 
-console.log(maiorNumero(65,455))
-
+console.log(maiorNumero(65, 455));
 
 //topico3
 
-function maiorNumero2(a,b,c) {
-  
-  if (a > b && a > c){
-    return a
-  } else if (b > c && b > a){
-    return b
-  } else{
-    return c
+function maiorNumero2(a, b, c) {
+  if (a > b && a > c) {
+    return a;
+  } else if (b > c && b > a) {
+    return b;
+  } else {
+    return c;
   }
 }
 
-console.log(maiorNumero2(54,4744,725))
-
+console.log(maiorNumero2(54, 4744, 725));
 
 //topico4
 
+function verifica(value) {
+  if (value > 0) {
+    return "Positivo";
+  } else if (value < 0) {
+    return "Negativo";
+  } else {
+    ("Zero");
+  }
+}
+
+console.log(verifica(0));
+//esta retornando undefined
+
+//topico5
+
+function somaDosAngulos(a, b, c) {
+  let somaDosAngulos = a + b + c;
+
+  if (somaDosAngulos >= 180) {
+    return "Sim é um triangulo";
+  } else if (a < 0 || b < 0 || c < 0) {
+    return "Opção invalida";
+  } else {
+    return "Não é um triangulo";
+  }
+}
+
+console.log(somaDosAngulos(60,-60,60))
