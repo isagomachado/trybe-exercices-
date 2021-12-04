f4c9c5980c11edd5b64cb1040a3c30bd3870bcc8
isagomachado <isa_gomes_machado@hotmail.com>
2021-12-03 21:09:32 -0300

    tentando começar o ultimo exercicio de fixação

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-fixacao/parte3.js
@@ -0,0 +1,5 @@
+const title = document.getElementById("header-container")
+title.style.backgroundColor = "#2fc18c"
+
+const ladoEsq = document.getElementsByClassName("teste")[0]
+ladoEsq.style.backgroundColor = "#fb75fb"
