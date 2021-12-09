52b664226d21193096b0201024213b62705c600f
isagomachado <isa_gomes_machado@hotmail.com>
2021-12-09 16:11:21 -0300

    ultimos ajustes

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-fixacao/parte3.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-fixacao/parte3.js
@@ -13,15 +13,6 @@ ladoDirCima.style.backgroundColor = "black"
 const testeDir = document.querySelectorAll(".no-emergency-tasks h3")
 testeDir[1].style.backgroundColor = "black"
 
-
-// let fundoDir = document.querySelectorAll(".tudo-direita")
-// fundoDir[0].style.backgroundColor = "#ff9f84"
-// fundoDir[1].style.backgroundColor = "#ff9f84"
-
-// let fundoEsq = document.querySelectorAll(".tudo-esquerda")
-// fundoEsq[0].style.backgroundColor = "#f9db5e"
-// fundoEsq[1].style.backgroundColor = "#f9db5e"
-
 const fundoDireita = document.querySelectorAll(".tudo-direita")
 fundoDireita[0].style.backgroundColor = "#ff9f84"
 
