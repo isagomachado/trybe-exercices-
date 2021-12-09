ac7a27daa89c346e47990a646f91166fb1803183
isagomachado <isa_gomes_machado@hotmail.com>
2021-12-09 16:10:58 -0300

    pintei o funfo certo da parte de urgentes e não urgentes

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-fixacao/parte3.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-fixacao/parte3.js
@@ -13,11 +13,20 @@ ladoDirCima.style.backgroundColor = "black"
 const testeDir = document.querySelectorAll(".no-emergency-tasks h3")
 testeDir[1].style.backgroundColor = "black"
 
+
+// let fundoDir = document.querySelectorAll(".tudo-direita")
+// fundoDir[0].style.backgroundColor = "#ff9f84"
+// fundoDir[1].style.backgroundColor = "#ff9f84"
+
+// let fundoEsq = document.querySelectorAll(".tudo-esquerda")
+// fundoEsq[0].style.backgroundColor = "#f9db5e"
+// fundoEsq[1].style.backgroundColor = "#f9db5e"
+
+const fundoDireita = document.querySelectorAll(".tudo-direita")
+fundoDireita[0].style.backgroundColor = "#ff9f84"
+
+const fundoEsquerda = document.querySelectorAll(".tudo-esquerda")
+fundoEsquerda[0].style.backgroundColor =  "#f9db5e"
+
 let rodape = document.getElementById("footer-container")
-rodape.style.backgroundColor = "#2fc18c"
-
-let fundoDir = document.querySelectorAll(".tudo-direita")
-fundoDir[0].style.backgroundColor = "#ff9f84"
-fundoDir[1].style.backgroundColor = "#ff9f84"
-
-let fundoEsq = document.querySelectorAll("tudo-esquerda")
+rodape.style.backgroundColor = "#003533"
