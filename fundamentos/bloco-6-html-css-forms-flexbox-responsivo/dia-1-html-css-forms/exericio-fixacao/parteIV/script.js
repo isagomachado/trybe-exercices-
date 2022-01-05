55d939bbc01f313c7ea45f63ae9de335f3160aa8
isagomachado <isa_gomes_machado@hotmail.com>
2022-01-05 10:40:58 -0300

    requisito 1 - link inutil

--- fundamentos/bloco-6-html-css-forms-flexbox-responsivo/dia-1-html-css-forms/exericio-fixacao/parteIV/script.js
+++ fundamentos/bloco-6-html-css-forms-flexbox-responsivo/dia-1-html-css-forms/exericio-fixacao/parteIV/script.js
@@ -2,3 +2,10 @@
 const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
+
+function linkInutil(event) {
+  event.preventDefault()
+
+}
+
+HREF_LINK.addEventListener('click', linkInutil)
