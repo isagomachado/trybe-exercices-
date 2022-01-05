4cb2be78b4576998a2404a1f6485d0dadb35c0b9
isagomachado <isa_gomes_machado@hotmail.com>
2022-01-05 11:34:00 -0300

    modificações

--- fundamentos/bloco-6-html-css-forms-flexbox-responsivo/dia-1-html-css-forms/exericio-fixacao/parteIV/script.js
+++ fundamentos/bloco-6-html-css-forms-flexbox-responsivo/dia-1-html-css-forms/exericio-fixacao/parteIV/script.js
@@ -6,6 +6,13 @@ const HREF_LINK = document.querySelector("#href");
 function linkInutil(event) {
   event.preventDefault()
 }
+function travaLetra(event) {
+  let teste = INPUT_TEXT.value
+  if (event.key !== a){
+    event.preventDefault()
+  }
+}
 
 HREF_LINK.addEventListener('click', linkInutil)
 INPUT_CHECKBOX.addEventListener('click', linkInutil)
+INPUT_TEXT.addEventListener('keypress', travaLetra)
