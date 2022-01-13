abc2418b188173728fe863c45ecdd49086c85c3e
isagomachado <isa_gomes_machado@hotmail.com>
2022-01-13 19:47:55 -0300

    exercicio de fixação sobre object keys - parte 2

--- fundamentos/bloco-7-introducao-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excessao-e-objetos/exericios-fixacao/objetos-parte1/script.js
+++ fundamentos/bloco-7-introducao-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excessao-e-objetos/exericios-fixacao/objetos-parte1/script.js
@@ -2,24 +2,24 @@
 ////////////////////////////////////////////
 
 const customer1 = {
-  firstName: 'Roberto',
+  firstName: "Roberto",
   age: 22,
-  job: 'Teacher',
+  job: "Teacher",
 };
 
 console.log(customer1);
 
-customer1.lastName = 'Faria';
+customer1.lastName = "Faria";
 console.log(customer1);
 
 const customer2 = {
-  firstName: 'Maria',
+  firstName: "Maria",
   age: 23,
-  job: 'Medic',
+  job: "Medic",
 };
 
 console.log(customer2);
-customer2['lastName'] = 'Silva';
+customer2["lastName"] = "Silva";
 console.log(customer2);
 
 ////////////////////////////////////////////////
@@ -27,19 +27,60 @@ console.log(customer2);
 ///////////////////////////////////////////////
 
 const customer = {
-  firstName: 'Roberto',
+  firstName: "Roberto",
   age: 22,
-  job: 'Teacher',
+  job: "Teacher",
 };
 
 const criaObj = (obj, key, value) => {
-
   let newKey = key;
   const keyy = value;
   obj[newKey] = keyy;
 
-  return console.log(obj)
+  return console.log(obj);
+};
+
+criaObj(customer, "lastName", "Ferreira");
+criaObj(customer, "fullName", `${customer.firstName} ${customer.lastName}`);
+
+////////////////////////////////////////////////////////////////
+// Exercicio 3
+////////////////////////////////////////////////////////////////
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+///////// função par mostrar as chaves do objeto/////////
+const showKeys = (obj) => Object.keys(obj);
+
+console.log(showKeys(student1));
+console.log(showKeys(student2));
+
+
+const skills = (student) => {
+  const getSkill = Object.keys(student); // gera uma array com todas as chaves
+  for (index in getSkill) {
+    console.log(`${getSkill[index]}, Nível: ${student[getSkill[index]]}`);
+  }
 }
 
-criaObj(customer, 'lastName', 'Ferreira')
-criaObj(customer, 'fullName', `${customer.firstName} ${customer.lastName}`)
+
+console.log('Estudante 1');
+skills(student1);
+
+console.log('Estudante 2');
+skills(student2);
+
+
