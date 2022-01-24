1b6d4016d1b5d8cecfb2c50ae3b46ac998eab5a9
isagomachado <isa_gomes_machado@hotmail.com>
2022-01-24 15:56:46 -0300

    exercicio de fixação sobre spread operator

--- fundamentos/bloco-8-higher-order-function-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios-fixacao/spread-operator.js
+++ fundamentos/bloco-8-higher-order-function-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios-fixacao/spread-operator.js
@@ -1,11 +1,14 @@
 // Faça uma lista com as suas frutas favoritas
-const specialFruit = ['', '', ''];
+const specialFruit = ['morango', 'uva', 'kiwi'];
 
 // Faça uma lista de complementos que você gostaria de adicionar
-const additionalItens = ['', '', ''];
+const additionalItens = ['melancia', 'mamão', 'maçã'];
 
 const fruitSalad = (fruit, additional) => {
   // Esreva sua função aqui
+  let newArray = [...fruit, ...additional]
+
+  return newArray
 };
 
 console.log(fruitSalad(specialFruit, additionalItens));
