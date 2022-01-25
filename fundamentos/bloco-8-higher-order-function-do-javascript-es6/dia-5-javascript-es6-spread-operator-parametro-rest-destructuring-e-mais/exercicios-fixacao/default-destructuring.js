da3262ac3b35b55bffb4e8a49c780ea7a90113fd
isagomachado <isa_gomes_machado@hotmail.com>
2022-01-25 09:42:32 -0300

    exercicio de fixaçao 3 - default destructring

--- fundamentos/bloco-8-higher-order-function-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios-fixacao/default-destructuring.js
+++ fundamentos/bloco-8-higher-order-function-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios-fixacao/default-destructuring.js
@@ -1,4 +1,4 @@
-const getNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+const getNationality = ({ firstName, nationality = 'brazilian'}) => `${firstName} is ${nationality}`;
 
 const person = {
   firstName: 'João',
