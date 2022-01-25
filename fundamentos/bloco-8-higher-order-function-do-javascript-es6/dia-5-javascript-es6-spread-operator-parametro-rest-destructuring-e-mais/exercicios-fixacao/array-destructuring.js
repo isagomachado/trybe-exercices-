e883b4de75b79cefd97db44f4040600f1058ed4d
isagomachado <isa_gomes_machado@hotmail.com>
2022-01-25 07:11:55 -0300

    exercicio 2 de fixação sobre array destructuring

--- fundamentos/bloco-8-higher-order-function-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios-fixacao/array-destructuring.js
+++ fundamentos/bloco-8-higher-order-function-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios-fixacao/array-destructuring.js
@@ -10,6 +10,20 @@ const [text, func] = saudacoes
 
 func(text)
 
-console.log(text)
-console.log(func)
+// console.log(text)
+// console.log(func)
+
+///////////// EXRCICIO 2//////////////
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+
+[comida, animal, bebida] = [animal, bebida, comida]
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
 
