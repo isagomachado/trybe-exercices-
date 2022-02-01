2fa1563b7e03b5142108b937adfe3c7bd96bccd6
isagomachado <isa_gomes_machado@hotmail.com>
2022-02-01 12:33:58 -0300

    instalação do node fetch

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-async-await/exercicios-do-dia/exercicio2/script.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-async-await/exercicios-do-dia/exercicio2/script.js
@@ -1,1 +1,20 @@
-const API_URL = `https://api.coincap.io/v2/assets`;
+const API_URL = 'https://api.coincap.io/v2/assets';
+
+const append = () => {
+  const coinList = document.querySelector('#coinList');
+
+}
+
+const howToExib = (obj) => `${obj.name} (${obj.symbol}): ${obj.priceUsd}`;
+
+
+const fetchCoin = () => {
+  const arrCoins = fetch(API_URL)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+
+
+  return arrCoins.map((coin) => howToExib(coin))
+}
+
+window.onload = () => fetchCoin();
