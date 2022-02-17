6ad9a16631518d4b4dbc4c5aaac19430c2856908
isagomachado <isa_gomes_machado@hotmail.com>
2022-02-17 11:05:46 -0300

    exercicios de fixação

--- desenvolvimento-front-end/bloco-10-introducao-ao-react/dia-1-introducao-front-end-react-hello-world-no-react/testando-meu-computador/src/App.js
+++ desenvolvimento-front-end/bloco-10-introducao-ao-react/dia-1-introducao-front-end-react-hello-world-no-react/testando-meu-computador/src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// const textJSX = 'JSX';
+
+//// configuração padrão da pagina quando instala 
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+// 
+// exercicio de fixação sobre JSX
+// 
+//       <section>
+//         <h1>Hello, { textJSX }</h1>
+//       </section>
+//       </header>
+
+//     </div>
+//   );
+// }
+
+//// codigo para o exercicio de fixação sobre reactDOM.render
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// function App() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString('pt', { hour12: true })}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+
+// setInterval(App, 1000);
+
+
+// export default App;
+
+
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>APP</h1>
     </div>
   );
 }
