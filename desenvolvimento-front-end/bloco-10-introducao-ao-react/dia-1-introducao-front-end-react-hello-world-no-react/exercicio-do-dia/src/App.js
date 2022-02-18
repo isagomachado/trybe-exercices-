5ea89318346efebb7a96c8c989f9bbfbb2e25bea
isagomachado <isa_gomes_machado@hotmail.com>
2022-02-18 11:07:04 -0300

    exercicio 1 - do dia

---
+++ desenvolvimento-front-end/bloco-10-introducao-ao-react/dia-1-introducao-front-end-react-hello-world-no-react/exercicio-do-dia/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const tarefas = ['Acordar', 'Tomar café','Tomar café', 'Tomar café', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <ul>{ tarefas.map((tarefa) => Task(tarefa)) }</ul>
+    );
+  }
+}
+
+export default App;
