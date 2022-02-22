e1358f3a730226e2d5b14a1e79a5f5d50ed3009f
isagomachado <isa_gomes_machado@hotmail.com>
2022-02-22 20:16:07 -0300

    exercicio de fixação

--- desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-1-componentes-com-estado-e-eventos/fixação/fancy-buttons/src/Button.js
+++ desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-1-componentes-com-estado-e-eventos/fixação/fancy-buttons/src/Button.js
@@ -4,19 +4,35 @@ import React from 'react';
 class Button extends React.Component {
   constructor() {
     super()
-    console.log('Um componente em construção')
-
+    this.state = {
+      numeroDeCliques: 0,
+      corDoBotão: 'green',
+    }
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick() {
-    console.log('Clicou no botão!');
-    console.log(this);
+    this.setState((estadoAnterior, _propsAnterior) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }))
+    if(this.state.numeroDeCliques % 2 !== 0) {
+      this.setState({
+        corDoBotão: 'green'
+      })
+    } else {
+      this.setState({
+        corDoBotão: 'white'
+      })
+    }
+    console.log(this.state.corDoBotão)
   }
 
   render() {
     return (
-      <button onClick={this.handleClick}>Meu botão</button>
+      <button
+        onClick={this.handleClick}
+        style={{ backgroundColor: this.state.corDoBotão}}
+      >{this.state.numeroDeCliques}</button>
     );
   }
 }
