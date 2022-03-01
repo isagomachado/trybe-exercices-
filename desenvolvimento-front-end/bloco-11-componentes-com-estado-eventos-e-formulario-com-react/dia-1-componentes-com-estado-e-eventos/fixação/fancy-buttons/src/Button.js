afbbaff8ec140df8012f8b9a3289266fc490d6f1
isagomachado <isa_gomes_machado@hotmail.com>
2022-03-01 18:56:44 -0300

    exercicio de fixação sobre eventos em react - deu certo

--- desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-1-componentes-com-estado-e-eventos/fixação/fancy-buttons/src/Button.js
+++ desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-1-componentes-com-estado-e-eventos/fixação/fancy-buttons/src/Button.js
@@ -4,35 +4,40 @@ import React from 'react';
 class Button extends React.Component {
   constructor() {
     super()
+
+    this.validate = this.validate.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
     this.state = {
       numeroDeCliques: 0,
       corDoBotão: 'green',
     }
-    this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick() {
-    this.setState((estadoAnterior, _propsAnterior) => ({
+    this.setState((estadoAnterior) => ({
       numeroDeCliques: estadoAnterior.numeroDeCliques + 1
-    }))
-    if(this.state.numeroDeCliques % 2 !== 0) {
+    }), this.validate)
+  }
+
+  validate() {
+    if(this.state.numeroDeCliques % 2 === 0) {
       this.setState({
         corDoBotão: 'green'
-      })
+      }, () => console.log(this.state.corDoBotão))
     } else {
       this.setState({
         corDoBotão: 'white'
-      })
+      }, () => console.log(this.state.corDoBotão))
     }
-    console.log(this.state.corDoBotão)
   }
 
   render() {
     return (
       <button
-        onClick={this.handleClick}
+        onClick={ this.handleClick }
         style={{ backgroundColor: this.state.corDoBotão}}
-      >{this.state.numeroDeCliques}</button>
+      >{this.state.numeroDeCliques} clicks</button>
     );
   }
 }
