0e4201d310f55d255d41f0248b4bb66a619909b1
isagomachado <isa_gomes_machado@hotmail.com>
2022-03-01 21:12:36 -0300

    exercicio de fixação sobre formularios

--- desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-2-formularios-no-react/fixacao/formulario/src/Form.jsx
+++ desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formulario-com-react/dia-2-formularios-no-react/fixacao/formulario/src/Form.jsx
@@ -4,35 +4,105 @@ class Form extends React.Component {
   constructor() {
     super();
 
+    this.handleChange = this.handleChange.bind(this);
 
+    this.state = {
+      usuario: '',
+      senha:'',
+      divaPopFavorita:'Selecione a melhor Diva do POP',
+      justificativa:'',
+      concordo: false,
+    }
+
+  }
+
+  handleChange(event) {
+    const { name } = event.target;
+    const value = event.target.type === 'checkbox'
+    ? event.target.checked
+    : event.target.value;
+
+    this.setState({
+      [name]: value
+    })
   }
   
   render() {
+    const {usuario, senha, divaPopFavorita, justificativa, concordo} = this.state
     return (
       <div>
         <h1>Exercicio sobre formulários em React</h1>
         <div className="form-content">
           <form>
-            <label>
-              Usuário:
-              <input type="text"></input>
-            </label>
+            <fieldset>
+              <legend>Login</legend>
+              <label htmlFor="usuario">
+                Usuário:
+                { ' ' }
+                <input
+                  type="text"
+                  name="usuario"
+                  id="usuario"
+                  onChange={ this.handleChange }
+                  value={ usuario }
+                />
+              </label>
+              <br />
+              <label htmlFor="senha">
+                Senha:
+                { ' ' }
+                <input
+                  type="password"
+                  name="senha"
+                  id="senha"
+                  onChange={ this.handleChange }
+                  value={ senha }
+                />
+              </label>
+            </fieldset>
             <br />
-            <label>
-              Senha:
-              <input type="password"></input>
-            </label>
-            <br />
-            <select name="divaPopFavorita">
+            <select
+              name="divaPopFavorita"
+              onChange={ this.handleChange }
+              value={ divaPopFavorita }
+            >
               <option>Selecione a melhor Diva do POP</option>
               <option value="Beyonce">Beyonce</option>
               <option value="Rihanna">Rihanna</option>
               <option value="Lady Gaga">Lady Gaga</option>
             </select>
             <br />
-            <label>
+            <label htmlFor="justificativa">
               Justifique sua escolha:
-              <textarea></textarea>
+              { ' ' }
+              <textarea
+                name="justificativa"
+                id="justificativa"
+                onChange={ this.handleChange }
+                value={ justificativa }
+              />
+            </label>
+            <br/>
+            <label htmlFor="foto">
+              Adicione sua foto favorita da sua diva
+              { ' ' }
+              <input
+                type="file"
+                name="foto"
+                id="foto"
+              />
+            </label>
+            <br />
+            <label htmlFor="concordo">
+              Li e aceito os termos
+              { ' ' }
+              <input
+                type="checkbox"
+                name="concordo"
+                id="concordo"
+                onChange={ this.handleChange }
+                checked={ concordo }
+              />
             </label>
             
           </form>
