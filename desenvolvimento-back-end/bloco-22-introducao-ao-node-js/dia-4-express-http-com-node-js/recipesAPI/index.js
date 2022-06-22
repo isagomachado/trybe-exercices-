11030b071048130f5e4276d1354cf3712d06ea6b
isagomachado <isa_gomes_machado@hotmail.com>
2022-06-22 17:04:33 -0300

    recebendo dados no body da requisição

--- desenvolvimento-back-end/bloco-22-introducao-ao-node-js/dia-4-express-http-com-node-js/recipesAPI/index.js
+++ desenvolvimento-back-end/bloco-22-introducao-ao-node-js/dia-4-express-http-com-node-js/recipesAPI/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.json());
 
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
-  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 45.0, waitTime: 25 },
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
@@ -17,6 +19,14 @@ const recipes = [
 // 	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 // ];
 
+// app.get('/validateToken', function (req, res) {
+//   const token = req.headers.authorization;
+
+//   if(token.length !== 16) return res.status(401).json({message: 'Invalid Token!'});
+
+//   res.status(200).json({message: 'Valid Token!'})
+// })
+
 app.get('/recipes', function (req, res) {
   const recipesSort = recipes.sort();
   res.json(recipesSort);
@@ -25,7 +35,19 @@ app.get('/recipes', function (req, res) {
 // app.get('/drinks', (req, res) => {
 //   const drinksSort = drinks.sort();
 //   res.json(drinksSort)
-// })
+// });
+
+app.post('/recipes', function (req, res) {
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price });
+  res.status(201).json({message: 'Recipe crated successfully'})
+});
+
+app.get('/recipes/search', (req, res) => {
+  const { name, maxPrice } = req.query;
+  const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < Number(maxPrice));
+  res.status(200).json(filteredRecipes);
+});
 
 app.get('/recipes/:id', function (req, res) {
   const { id } = req.params;
