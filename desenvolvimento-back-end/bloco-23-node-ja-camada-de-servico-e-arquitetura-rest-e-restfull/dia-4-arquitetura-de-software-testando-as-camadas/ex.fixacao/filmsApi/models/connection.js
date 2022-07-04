999344ea8bbc537684201a22f08022ad440707cc
isagomachado <isa_gomes_machado@hotmail.com>
2022-07-04 15:55:58 -0300

    teste camada model

---
+++ desenvolvimento-back-end/bloco-23-node-ja-camada-de-servico-e-arquitetura-rest-e-restfull/dia-4-arquitetura-de-software-testando-as-camadas/ex.fixacao/filmsApi/models/connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'igm@1996',
+  database: 'model_exemple'});
+
+module.exports = connection;
