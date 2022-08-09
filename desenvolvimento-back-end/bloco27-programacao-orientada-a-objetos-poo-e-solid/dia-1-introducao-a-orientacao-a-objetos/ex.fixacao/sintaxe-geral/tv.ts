c6374ed0930b7cf08dc98750e44f5d0332419575
isagomachado <isa_gomes_machado@hotmail.com>
2022-08-09 16:33:54 -0300

    ex. fixacao sintaxe geral

---
+++ desenvolvimento-back-end/bloco27-programacao-orientada-a-objetos-poo-e-solid/dia-1-introducao-a-orientacao-a-objetos/ex.fixacao/sintaxe-geral/tv.ts
@@ -0,0 +1,23 @@
+class TV {
+  brand: string;
+  size: string;
+  resolution: string;
+  connections: string;
+  // connectedTo: string;
+
+  constructor(b: string, s: string, r: string, c:string) {
+    console.log(`Creating tv da marca: ${b}`);
+    this.brand = b;
+    this.size = s;
+    this.resolution = r;
+    this.connections = c;
+  }
+
+  turnOn() {
+    console.log(`${this.brand}, ${this.size}, ${this.resolution}, ${this.connections} `);
+  }
+}
+
+
+const tv1 = new TV('sansung', '60 polegadas', '4k', 'HDMI');
+tv1.turnOn();
